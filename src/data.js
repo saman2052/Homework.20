08368c50295175a86a0111d14e0049e40c5fb22e
saman2052 <tire_ashkani@yahoo.com>
2021-01-08 00:31:59 -0500

    Uploading homework 20

---
+++ src/data.js
@@ -0,0 +1,79 @@
+export default
+    {
+        
+        name :'Saman Movaffaghi',
+        headerTagline: [
+                        'I am Saman Movaffaghi.',
+                        'a full stack web developer',
+                        'from Washington D.c.'
+    ],
+        contactEmail:'samanmo404@gmail.com',
+        resume: "./resume.zip",
+        abouttext: "I am a web Developer with a love of coding, computers and learning. Great at problem-solving and taking on challenges. Knowledgeable in the web development technologies of HTML, CSS, Bootstrap, Javascript, and others. Communication with other team members is a high priority for me as we share a common passion for creating dynamically engaging applications to progress our human experience in the modern world.",
+        aboutImage:'./backgpic.jpg',
+       ShowAboutImage:true,
+ 
+       projects:[
+        {
+            id: 1,
+            title: 'PROJECT 2: COVID-19 TRACKING APP',
+            service: 'This is an app created to track and inform people of Covid-19 cases, infections and deaths in each U.S state.',
+            imageSrc: "./design.png",
+            url: 'https://project2gwu.herokuapp.com/'
+        },
+        {
+            id: 2,
+            title: 'VACATION PLANNER',
+            service: 'vacation planner is a site used for planning trips anywhere in the world. The site showcases hotels, restuarants, tourist destinations as well as the weather of any location of your choosing.',
+            imageSrc: "./catsndogs.png",
+            url: 'https://saman2052.github.io/vacation_planner/'
+        },
+
+           {
+            id: 3,
+            title:'Password Generator', 
+             service:'An application that generates a random password based on user criteria. This app runs in the browser and features dynamically updated HTML and CSS powered by JavaScript code.', 
+             imageSrc:"./pass.png",
+             url:'https://saman2052.github.io/homework.3three/'
+            },
+            {
+                id: 4,
+                title: 'Note Taker',
+                service: 'Note taking application designed to write, save, and delete notes, written to and retrieved from a backend JSON file and utilizing Express.',
+                imageSrc: "./rmgen.png",
+                url: 'https://samansnotes.herokuapp.com/'
+            },
+            { 
+                id: 5,
+                title: 'Eat da burger',
+                service: 'MySQL, Node, Express, Handlebars and a ORM',
+                imageSrc: "./burger.png",
+                url: 'https://intense-reef-34889.herokuapp.com/'
+            },
+            {
+                id: 6,
+                title: 'Weather App',
+                service: 'Created a dashboard that uses the OpenWeather API to retrieve weather data for cities that are searched for by user.',
+                imageSrc: "./weather.png",
+                url: 'https://saman2052.github.io/weatherdashboard.1/'
+           }
+
+                        
+                
+               
+        ],
+        social: [
+
+            {   name:'Github',
+                url:'https://github.com/saman2052/'},
+            {
+                name: 'Linkedin',
+                url: 'https://www.linkedin.com/in/saman-movaffaghi-034154201/'
+            },
+            {
+                name: 'Instagram',
+                url: 'https://www.instagram.com/samandapersian/'
+            }
+
+        ]
+    }
